c15fe7a5d1c1d65b5263a71e6dcb20532e017930
wanyingl09 <wli30@stuy.edu>
2023-06-05 09:26:48 -0400

    tbc

--- app/static/js/client.js
+++ app/static/js/client.js
@@ -4,6 +4,20 @@
 // P05
 
 
+// html manipulating ---------------------------------------------------------------------
+var UnhiddenUM = document.getElementById("btnUnhiddenUM");
+var hidden_and_unhidden = () => {
+  var um = document.getElementById("userManual");
+  if (um.style.display === "none") {
+    um.style.display = "block";
+  }
+  else {
+    um.style.display = "none";
+  }
+};
+UnhiddenUM.addEventListener("click", un_hidden);
+
+
 // connect client ----------------------------------------------------------------------
 
 // Create WebSocket connection.
@@ -18,3 +32,4 @@ socket.addEventListener("open", (event) => {
 socket.addEventListener("message", (event) => {
   console.log("Message from server ", event.data);
 });
+
